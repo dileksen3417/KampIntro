b04b9e7b0d3e1c3382fee5db464b7ae338d4113b
Dilek ŞEN <dileksen3417@gmail.com>
2021-01-19 01:14:27 +0300

    added examples of ref and out keywords

---
+++ RefAndOutKw/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace RefAndOutKw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**********Ref Kullanımı:***********");
+            int x = 10;
+            int y = 15;
+            Console.WriteLine("Ref kullanımından önce x değeri: " + x);
+            Add(ref x, y);
+            Console.WriteLine("Ref kullanımından sonra x değeri: " + x);
+
+            Console.WriteLine("\n**********Out Kullanımı:***********");
+            int a;
+            int b = 200;
+            Console.WriteLine("Ref kullanımından önce a değeri: ");
+            Add2(out a, b);
+            Console.WriteLine("Ref kullanımından sonra a değeri: " + a);
+        }
+
+        static int Add(ref int x, int y)
+        {
+            x = 30;
+            return x + y;
+        }
+
+        static int Add2(out int a, int b)
+        {
+            a = 50;
+            return a + b;
+        }
+
+    }
+}
