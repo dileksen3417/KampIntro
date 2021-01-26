4affcb2a2de9dc87d96c11fe24aad1113eb32e96
Dilek ŞEN <dileksen3417@gmail.com>
2021-01-26 23:45:14 +0300

    database example added for Interface

---
+++ Interface2/ICustomerDal.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interface2
+{
+    interface ICustomerDal
+    {
+        void Add();
+        void Delete();
+        void Update();
+    }
+
+    public class MySqlCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Mysql added.");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Mysql deleted.");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Mysql updated.");
+        }
+    }
+
+    public class OracleCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Oracle added.");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Oracle deleted.");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Oracle updated.");
+        }
+    }
+
+    class CustomerManager{
+        public void Add(ICustomerDal customerDal)
+        {
+            customerDal.Add();
+        }
+
+    }
+
+}
---
+++ Interface2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Interface2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonalManager manager = new PersonalManager();
+            manager.Add(new Customer { Id = 1, FirstName = "Dilek", LastName = "Şen", CustomerDepartment = "Yazılım" });
+            Student student = new Student
+            {
+                Id = 2,
+                FirstName = "Güney",
+                LastName = "Şen",
+                StudentAddress = "Yenidoğan"
+            };
+            manager.Add(student);
+
+            ///veritabanı örnegimiz ; 
+            Console.WriteLine("\n\nVeritabanı İşlemleri:" + "\n*****************");
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(new MySqlCustomerDal());
+            customerManager.Add(new OracleCustomerDal());
+            // her iki databse ile de ekleme işlemini yapabilirim.
+
+        }
+    }
+
+    interface IPerson{
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+    }
+
+    class Student : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StudentAddress { get; set; }
+    }
+
+    class Customer: IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CustomerDepartment { get; set; }
+    }
+
+    class PersonalManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine(person.FirstName + " sisteme eklendi.");
+            
+        }
+    }
+}
