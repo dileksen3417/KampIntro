0f4297b537c2af065404389e532eff5373e182c0
Dilek ŞEN <dileksen3417@gmail.com>
2021-01-27 23:56:03 +0300

    inheritance example added

---
+++ Inheritance(Kalıtım)/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Inheritance_Kalıtım_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[3]
+            {
+                new Student{FirstName="Dilek" },
+                new Customer{FirstName="Altuğ"},
+                new Person{FirstName="Güneş" } //interfaceden farkı
+            };
+
+            foreach (Person person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+
+    class Student : Person
+    {
+        public string SchoolNumber { get; set; }
+    }
+
+    class Customer : Person
+    {
+        public string CustomerNumber { get; set; }
+    }
+}
---
+++ InterfaceExample3/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace InterfaceExample3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IWorker için tüm çalışanlarıma çalışma emri veriyorum:
+            IWorker[] workers = new IWorker[3]
+            {
+                new Manager(),
+                new Worker(),
+                new Robot()
+            };
+            foreach (var worker in workers)
+            {
+                worker.Work();
+            }
+
+            //IEat interfaceini impl eden tüm çalışanlarım için yemek yeme izni veriyorum:
+            IEat[] eats = new IEat[2]
+            {
+                new Manager(),
+                new Worker()
+            };
+            foreach (var eat in eats)
+            {
+                eat.Eat();
+            }
+            //bu işlemi robotlar için yapamam çünkü onlar IEAT interfaceinden türemediler böyle bir opsiyonları yok.
+
+        }
+    }
+
+    interface IWorker
+    {
+        void Work();
+    }
+
+    interface IEat
+    {
+        void Eat();
+    }
+
+    interface ISallary
+    {
+        void GetSalary();
+    }
+
+    class Manager : IWorker, IEat, ISallary
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Manager yemegini yedi.");
+        }
+
+        public void GetSalary()
+        {
+            Console.WriteLine("Manager maaşını aldı.");
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("Manager çalıştı.");
+        }
+    }
+
+    class Worker : IWorker, ISallary, IEat
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Worker yemegini yedi.");
+        }
+
+        public void GetSalary()
+        {
+            Console.WriteLine("Worker maaşını aldı.");
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("Worker çalıştı.");
+        }
+    }
+
+    class Robot : IWorker
+    {
+        public void Work()
+        {
+            Console.WriteLine("Robot çalıştı.");
+        }
+    }
+}
