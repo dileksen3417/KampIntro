01dd397b39e0b2c9762a54b1c8fdfa38cb2746c1
Dilek ŞEN <dileksen3417@gmail.com>
2021-02-13 20:09:01 +0300

    added NorthwindContext operations

---
+++ EntityFrameworkDemo/NorthwindContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkDemo
+{
+    public class NorthwindContext:DbContext
+    {
+        //veritabanımızın olduğu yeri bağlayalım
+        //override yani üzerine yazmak, OnConfiguring methodu DbContext classının içindeki bir methoddur ve tipi virtualdır
+        //virtual imzaya sahip bir method override keywordu ile içerisi değiştirilerek kullanılabilir.
+
+        //db bağlantısını sağlayalım
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Northwind;Trusted_Connection=true");
+
+        }
+        //db tabloları ile classlarımızı ilişkilendirelim
+        public DbSet<Product> Products { get; set; }  //<Product> : bizim projemizdeki class, Products: dbdeki tablonun adı
+        public DbSet<Personel> Personels { get; set; }
+
+        //Personel classım ile dbdeki Employees'i ilişkilendirebilmem için DbContext classının bir virtual methodu olan 
+        //OnModelCreating methodunu override ederek kullanıyorum:
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Personel>().ToTable("Employees"); // <Personel> isimli classımı git Employees tablosuyla bagla
+            //kolonları baglayalım
+            modelBuilder.Entity<Personel>().Property(p => p.Id).HasColumnName("EmployeeID");
+            modelBuilder.Entity<Personel>().Property(p => p.Name).HasColumnName("FirstName");
+            modelBuilder.Entity<Personel>().Property(p => p.Surname).HasColumnName("LastName");
+
+        }
+
+    }
+}
---
+++ EntityFrameworkDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace EntityFrameworkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //veritabanı bağlantımızı oluşturalım:
+            NorthwindContext northwindContext = new NorthwindContext();
+
+            GetAllProducts(northwindContext);
+            GetProductsByCategory(northwindContext,1);
+        }
+
+        private static void GetProductsByCategory(NorthwindContext northwindContext,int categoryId)
+        {
+            //Verilen kategory id 'ye göre product nameleri listler
+            var result = northwindContext.Products.Where(p => p.CategoryId == categoryId);
+
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+
+        private static void GetAllProducts(NorthwindContext northwindContext)
+        {
+            //northwindContext ile dbmize bağlanıyoruz. .Products dediğimizde Products tablosunu çağırırız. 
+            //Bu Products tablosundaki ProductName kolonlarını yazdıralım:
+            foreach (var product in northwindContext.Products)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
